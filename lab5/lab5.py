5db193f551b654399b9eda274080d89d32e47155
jawzal19-meet <jawzal19@meet.mit.edu>
2017-11-27 19:01:34 +0200

    i uploaded my code!

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -2,24 +2,35 @@ from turtle import Turtle
 import random
 
 
-class Square(Turtle):
-	def__init__(self,size)
-		Turtle.__init__(slef)
-		self.shapesize(size)
-		self.shape("square")
+#	def__init__(self,size)
+#		Turtle.__init__(slef)
+#		self.shapesize(size)
+#		self.shape("square")
 
-	def random_color():
-		r = random.randint(0, 256)
-		g = random.randint(0, 256)
-		b = random.randint(0, 256)
-		self.color(r, g, b)
+#	def random_color():
+#		r = random.randint(0, 256)
+#		g = random.randint(0, 256)
+#		b = random.randint(0, 256)
+#		self.color(r, g, b)
 
 
 #EXTRAAAAAAA
 #############################
 
 class Hexagon(Turtle):
-	def__init__(self,size)
-	Turtle.__init__(self)
-	self.shapesize(size)
-	self.shape()
+	def__init__(self,size):
+		Turtle.__init__(self)
+		self.begin_poly()
+		for i in range(6):
+			self.forward(size)
+			self.left(360/6)
+		self.end_poly()
+		turtle.register_shape("hexagon", self.get_poly())
+		self.shape("hexagon")
+		self.shapesize(size)
+
+hexagon = Hexagon(10)
+
+turtle.mainloop()
+
+
